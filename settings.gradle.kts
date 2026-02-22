82a19c44ecb3de85409ce3a9002361b26e0d17a0
Viga Bots <133951124+vigarepo2@users.noreply.github.com>
2026-02-22 12:45:32 +0530

    Create settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+// filepath: settings.gradle.kts
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "FIR Portal"
+include(":app")
